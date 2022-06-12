f17d89144430ec26b2723b28cab97bf540f0486e
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-06-12 11:44:24 -0400

    Layout form and add basic interactivity

--- canvas.js
+++ canvas.js
@@ -5,14 +5,14 @@ const ctx = graph.getContext('2d')
 ctx.fillStyle = 'rgb(200,0,0)'
 ctx.fillRect(10, 10, 50, 50)
 
-ctx.clearRect(15,15, 40, 40)
+ctx.clearRect(15, 15, 40, 40)
 
-ctx.strokeStyle = "rgb(0,0,200)"
-ctx.strokeRect(20,20, 30, 30)
+ctx.strokeStyle = 'rgb(0,0,200)'
+ctx.strokeRect(20, 20, 30, 30)
 
-ctx.fillStyle = "rgb(0,0,0)"
-ctx.beginPath();
-ctx.moveTo(210,60);
-ctx.lineTo(0,0);
-ctx.closePath();
-ctx.stroke();
+ctx.fillStyle = 'rgb(0,0,0)'
+ctx.beginPath()
+ctx.moveTo(210, 60)
+ctx.lineTo(0, 0)
+ctx.closePath()
+ctx.stroke()
--- index.js
+++ index.js
@@ -20,16 +20,17 @@
  * @param {Function} callback any callback that behavior like map
  * @returns {Array}	mapped matched array
  */
-function pairing(array, callback) {
-  let result = []
+function pairing (array, callback) {
+  const result = []
   for (let i = 0; i < array.length; i++) {
     for (let k = 0; k < array.length; k++) {
       if (i === k || i > k) continue
       result.push(callback(array[i], array[k]))
     }
   }
-  return result.filter(e => e !== undefined)
+  return result.filter((e) => e !== undefined)
 }
+
 /**
  * Función para validar las coordenadas.
  * Será True siempre que se cumplan TODAS las condiciones.
@@ -38,11 +39,11 @@ function pairing(array, callback) {
  * @param {Number} y
  * @returns {Boolean}
  */
- function coorValidation(conditions, x, y) {
+function coorValidation (conditions, x, y) {
   const valid = []
-  conditions.forEach(el => {
+  conditions.forEach((el) => {
     const { a, b, c, d } = el
-    const operation = ((a * x) + (b * y))
+    const operation = a * x + b * y
     const equalities = {
       0: operation == c,
       1: operation >= c,
@@ -50,15 +51,16 @@ function pairing(array, callback) {
     }
     valid.push(equalities[d])
   })
-  return valid.every(e => e === true)
+  return valid.every((e) => e === true)
 }
+
 /**
  * Función para hallar la intercección entre las restricciones
  * recibe un arreglo de objeto con las propiedades a, b, c y i
  * @param {Array<Equation>} conditions
  * @returns {Array<Intersection>} coordenadas de la intersección
  */
-function getIntersections(conditions) {
+function getIntersections (conditions) {
   return pairing(conditions, (r1, r2) => {
     const { a, b, c, i } = r1
     const { a: a2, b: b2, c: c2, i: i2 } = r2
@@ -72,41 +74,13 @@ function getIntersections(conditions) {
 /**
  * @type {Equation} a - coeficiente; b - coeficiente; c - término independiente; d - sentido de la desigualdad; i - indice de la restricción
  */
-const y = { a: 1, b: 0, c: 0, i: 0, d:1 }
+const y = { a: 1, b: 0, c: 0, i: 0, d: 1 }
 /**
  * @type {Equation} a - coeficiente de x; b - coeficiente de y; c - término independiente; d - sentido de la desigualdad; i - indice de la restricción
  */
-const x = { a: 0, b: 1, c: 0, i: 0, d:1 }
+const x = { a: 0, b: 1, c: 0, i: 0, d: 1 }
 // array de ecuación X Y
 const ejes = [x, y]
-// Función objetivo Z = ax + by;
-// funcion objetivo  ? = {a:2, b:3}
-// formula objetivo = a*x + b*y
-// bucle para probar y almacenar los resultados en un array y luego buscar el menor de ellos.
-// además he de probar que sean válidos dentro de las restricciones.
-// formula de la restricción: r = a*x + b*y  1 >= | <= 2 c
-
-// testing ....
-const restricciones = [
-  { a: 20, b: 30, c: 6000, i: 1, d: 2 },
-  { a: 20, b: 10, c: 4800, i: 2, d: 2 }
-]
-const maximizar = true
-const objFunction = { a: 1.5, b: 1, i: 0, d: 0 }
-// Ejercicio de mesas
-let mesas = [...restricciones, ...ejes]
-
-//ejercicio random de 3 ecuaciones que no se encuentran interceptadas en el cuadrante I
-// r1: 10x+2y >= 150
-// a: 10, b:2 , c:150, i:1
-let r1 = { a: 10, b: 2, c: 150, i: 1 }
-// r2: 2x-8y >= 90
-// a: 2, b:8 , c:90, i:1
-let r2 = { a: 2, b: -8, c: 90, i: 1 }
-// r3: x+y <= 10
-// a: 1, b:1 , c:10, i:2
-let r3 = { a: 1, b: 1, c: 10, i: 2 }
-let r = [r1, r2, r3, x,y]
 
 /**
  * Función para resolver la función objetivo con cada coordenada.
@@ -115,26 +89,27 @@ let r = [r1, r2, r3, x,y]
  * @param {Array<Equation>} conditions Array of conditions of the problem
  * @returns an array of valid intersections with Z value
  */
-function resolveFnObjective(objFunction, conditions) {
+function resolveFnObjective (objFunction, conditions) {
   const res = []
-	const intersections = getIntersections(conditions)
-	intersections.forEach(coordinate => {
-		const { x, y } = coordinate
-		const { a, b } = objFunction
-		const z = a*x + b*y
-			if(coorValidation(conditions, x, y)) 
-				 res.push({x, y, z})
-	})
+  const intersections = getIntersections(conditions)
+  intersections.forEach((coordinate) => {
+    const { x, y } = coordinate
+    const { a, b } = objFunction
+    const z = a * x + b * y
+    if (coorValidation(conditions, x, y)) res.push({ x, y, z })
+  })
   return res
 }
 
-const results = resolveFnObjective(objFunction, mesas)
+export function resolveProblem (objFn, restrictions, maximize) {
+  restrictions = [...restrictions, ...ejes]
+  const results = resolveFnObjective(objFn, restrictions)
 
-if(maximizar) {
-  results.sort((a, b) => b.z - a.z)
-} else{
-  results.sort((a, b) => a.z - b.z)
+  if (maximize) {
+    results.sort((a, b) => b.z - a.z)
+  } else {
+    results.sort((a, b) => a.z - b.z)
+  }
+
+  return results[0]
 }
-
-console.log("La Solución optima es:" + JSON.stringify(results[0]))
-console.log(results)
